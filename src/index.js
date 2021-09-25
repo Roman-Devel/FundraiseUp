b5166d7a6072db6797fbaba00ecd18811e768e15
Melnikov-Roman <melnikov.roman.lang@gmail.com>
2021-09-26 00:37:58 +0600

    form done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles/main.scss'
+
+const rangeSize = document.getElementById("rangeSize"),
+	  trackSize = document.querySelector('.trackSize'),
+	  rangeRadius = document.getElementById("rangeRadius"),
+	  trackRadius = document.querySelector('.trackRadius')
+
+rangeSize.addEventListener('input', () => trackSize.style.width = rangeSize.value + '%')
+rangeRadius.addEventListener('input', () => trackRadius.style.width = rangeRadius.value + '%')
+
+document.querySelector('button[type="submit"]').addEventListener('click', (event) => {
+	event.preventDefault()
+})
